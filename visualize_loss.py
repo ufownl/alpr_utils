347bb780a6f68896c058b9136f5dec3137c88fa7
RangerUFO <ufownl@gmail.com>
2019-09-25 02:07:05 +0800

    Unify the x-coordinate of the loss charts

--- visualize_loss.py
+++ visualize_loss.py
@@ -4,25 +4,32 @@ import matplotlib.pyplot as plt
 
 
 def visualize(lines):
-    re_batch = re.compile(".* batch_loss (\S+).*")
-    re_epoch = re.compile(".* training_loss (\S+).* validating_loss (\S+).*")
+    regex = re.compile("^\[Epoch ([0-9]+)  Batch ([0-9]+)\]  batch_loss (\S+).*")
+    batch_x = []
     batch_loss = []
+    for line in lines:
+        m = regex.match(line)
+        if m:
+            batch_x.append((int(m.group(1)), int(m.group(2))))
+            batch_loss.append(float(m.group(3)))
+    batches = max(batch_x, key=lambda x: x[1])[1]
+    batch_x = [epoch + batch / batches for epoch, batch in batch_x]
+    regex = re.compile("^\[Epoch ([0-9]+)\]  training_loss (\S+)  validating_loss (\S+).*")
+    epoch_x = []
     training_loss = []
     validating_loss = []
     for line in lines:
-        m = re_batch.match(line)
+        m = regex.match(line)
         if m:
-            batch_loss.append(float(m.group(1)))
-        m = re_epoch.match(line)
-        if m:
-            training_loss.append(float(m.group(1)))
-            validating_loss.append(float(m.group(2)))
+            epoch_x.append(int(m.group(1)))
+            training_loss.append(float(m.group(2)))
+            validating_loss.append(float(m.group(3)))
     plt.subplot(2, 1, 1)
-    plt.plot(np.array(batch_loss), label="batch loss")
+    plt.plot(np.array(batch_x), np.array(batch_loss), label="batch loss")
     plt.grid(True)
     plt.subplot(2, 1, 2)
-    plt.plot(np.array(training_loss), label="training loss")
-    plt.plot(np.array(validating_loss), label="validating loss")
+    plt.plot(np.array(epoch_x), np.array(training_loss), label="training loss")
+    plt.plot(np.array(epoch_x), np.array(validating_loss), label="validating loss")
     plt.grid(True)
     plt.legend()
     plt.show()
