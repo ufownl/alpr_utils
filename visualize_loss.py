898ce3b16587ef0d9fa8845a74f3a18de69bad66
RangerUFO <ufownl@gmail.com>
2019-09-24 18:35:08 +0800

    Add a chart to visualize the batch loss

--- visualize_loss.py
+++ visualize_loss.py
@@ -4,15 +4,23 @@ import matplotlib.pyplot as plt
 
 
 def visualize(lines):
-    regex = re.compile(".* training_loss (\S+).* validating_loss (\S+).*")
+    re_batch = re.compile(".* batch_loss (\S+).*")
+    re_epoch = re.compile(".* training_loss (\S+).* validating_loss (\S+).*")
+    batch_loss = []
     training_loss = []
     validating_loss = []
-    min_ppl = float("inf")
     for line in lines:
-        m = regex.match(line)
+        m = re_batch.match(line)
+        if m:
+            batch_loss.append(float(m.group(1)))
+        m = re_epoch.match(line)
         if m:
             training_loss.append(float(m.group(1)))
             validating_loss.append(float(m.group(2)))
+    plt.subplot(2, 1, 1)
+    plt.plot(np.array(batch_loss), label="batch loss")
+    plt.grid(True)
+    plt.subplot(2, 1, 2)
     plt.plot(np.array(training_loss), label="training loss")
     plt.plot(np.array(validating_loss), label="validating loss")
     plt.grid(True)
